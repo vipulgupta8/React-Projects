eb2fc4cfa7633ebe17b117dd814549198ff71e7b
vipul Gupta <vipulgupta863@gmail.com>
2024-06-03 20:44:44 +0530

    push

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+const App = () => {
+  return (
+    <div className='royal'>
+      <Sidebar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
